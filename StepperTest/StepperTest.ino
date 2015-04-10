9758da584203496d5486cd64720907acbe97bb86
choard1895 <hoard.c@husky.neu.edu>
2015-04-10 14:51:21 -0500

    Correct version of stepper test

--- StepperTest/StepperTest.ino
+++ StepperTest/StepperTest.ino
@@ -1,32 +1,22 @@
 
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
 
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-
-Adafruit_StepperMotor *motorA = AFMS.getStepper(200, 1);
-Adafruit_StepperMotor *motorB = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.begin(9600);
   Serial.println("Stepper test!");
 
-  AFMS.begin();  // create with the default frequency 1.6KHz
-
-  motorA->setSpeed(200); 
-  motorB->setSpeed(200); 
-  motorA->step(10, FORWARD, SINGLE);
+  AFMS.begin(); 
+  myMotor->setSpeed(100);
 }
 
 void loop() {
-  Serial.println("Single coil steps");
-
-//motorA->release();
-  motorA->step(200, FORWARD, DOUBLE ); 
-//  motorB->release();
-
-
+//   myMotor->step(200, BACKWARD, DOUBLE);  // this opens the nosecone
+   myMotor->step(200, FORWARD, DOUBLE);  // this closes the nosecone
 }
