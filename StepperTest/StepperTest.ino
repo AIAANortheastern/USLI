d8b9a25204f102441a9ce1cc1ff537d026400f7b
choard1895 <Jon@Jonathans-MacBook-Pro.local>
2015-04-10 15:55:39 -0500

    New stepper control panel

--- StepperTest/StepperTest.ino
+++ StepperTest/StepperTest.ino
@@ -1,22 +1,48 @@
 
+
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_StepperMotor *motorA = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *motorB = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *myMotor;
 
+char cmd = 'x';
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Stepper test!");
-
-  AFMS.begin(); 
-  myMotor->setSpeed(100);
+  AFMS.begin();
+  motorA->setSpeed(100);
+  motorB->setSpeed(100);
+  myMotor = motorA;
 }
 
 void loop() {
-//   myMotor->step(200, BACKWARD, DOUBLE);  // this opens the nosecone
-   myMotor->step(200, FORWARD, DOUBLE);  // this closes the nosecone
+  while (Serial.available()) {
+    cmd = Serial.read();
+    if (cmd == 'f') {
+      Serial.println("Forward");
+    } else if (cmd == 'b') {
+      Serial.println("Backward");
+    } else if (cmd == '1') {
+      Serial.println("Motor 1 Selected");
+      myMotor = motorA;
+    } else if (cmd == '2') {
+      Serial.println("Motor 2 Selected");
+      myMotor = motorB;
+    } else {
+      Serial.println("Halt");
+    }
+  }
+
+  if (cmd == 'f') {
+    myMotor->step(10, FORWARD, DOUBLE);  // this closes the nosecone
+  }
+
+  if (cmd == 'b') {
+    myMotor->step(10, BACKWARD, DOUBLE);  // this opens the nosecone
+  }
 }
